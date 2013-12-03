0629df250e7603fd9a06c1cb4c2efa3ae39ad8f4
Xi Yu <xiyu@Xs-MacBook-Air.local>
2013-12-03 00:15:40 -0500

    Create database and user login
    SQLite and login modules

--- microblog/__init__.py
+++ microblog/__init__.py
@@ -1,7 +1,20 @@
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from microblog import settings
+from settings import basedir
 from flask import Flask
-import settings
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object(settings)
+db = SQLAlchemy(app)
 
-import microblog.views
+lm = LoginManager()
+lm.setup_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+
+from microblog import views, models
